69538680c5649d6edd3624834f645f3d17888b0b
dihnhuunam <dihnhuunam@gmail.com>
2026-06-15 00:11:35 +0700

    [FEAT] Update script for changing default tools

--- install.sh
+++ install.sh
@@ -25,30 +25,78 @@ error() {
   echo -e "${RED}[ERROR]${NC} $1"
 }
 
+run_script() {
+  local script_path="$1"
+
+  if [[ -x "$script_path" ]]; then
+    "$script_path"
+  elif [[ -f "$script_path" ]]; then
+    bash "$script_path"
+  else
+    error "Script not found: $script_path"
+    exit 1
+  fi
+}
+
 info "Installing Ubuntu packages..."
-./packages/ubuntu.sh
+run_script "./packages/ubuntu.sh"
+
+info "Setting qpdfview as default PDF viewer..."
+
+if command -v qpdfview >/dev/null 2>&1; then
+  xdg-mime default qpdfview.desktop application/pdf
+  success "qpdfview is set as the default PDF viewer."
+else
+  warn "qpdfview not found. Skipping default PDF viewer setup."
+fi
+
+info "Restarting IBus..."
+
+if command -v ibus >/dev/null 2>&1; then
+  ibus restart || warn "IBus restart failed. You may need to log out and log in again."
+else
+  warn "IBus is not installed."
+fi
 
 info "Installing Fonts..."
-./packages/font.sh
+run_script "./packages/font.sh"
 
-info "Installing Zsh ..."
-./packages/zsh.sh
+info "Installing Zsh..."
+run_script "./packages/zsh.sh"
 
 info "Installing WezTerm..."
-./packages/wezterm.sh
+run_script "./packages/wezterm.sh"
 
 info "Setting WezTerm as default terminal..."
 
 if command -v gsettings >/dev/null 2>&1 && command -v wezterm >/dev/null 2>&1; then
-  gsettings set org.gnome.desktop.default-applications.terminal exec wezterm
-  gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
-  success "WezTerm is set as the default terminal."
+  if gsettings writable org.gnome.desktop.default-applications.terminal exec >/dev/null 2>&1; then
+    gsettings set org.gnome.desktop.default-applications.terminal exec wezterm
+    gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
+    success "WezTerm terminal preference was updated."
+  else
+    warn "GNOME terminal setting is not available on this desktop environment."
+  fi
 else
   warn "Cannot set default terminal automatically. gsettings or wezterm not found."
 fi
 
 info "Creating config files..."
-cp -r ./zsh/.zshrc ~/.zshrc
-cp -r ./wezterm/.config/wezterm ~/.config/
+
+mkdir -p "$HOME/.config"
+
+if [[ -f "./zsh/.zshrc" ]]; then
+  cp "./zsh/.zshrc" "$HOME/.zshrc"
+  success "Zsh config copied."
+else
+  warn "Zsh config not found: ./zsh/.zshrc"
+fi
+
+if [[ -d "./wezterm/.config/wezterm" ]]; then
+  cp -r "./wezterm/.config/wezterm" "$HOME/.config/"
+  success "WezTerm config copied."
+else
+  warn "WezTerm config directory not found: ./wezterm/.config/wezterm"
+fi
 
 success "Done. Please restart the terminal."
